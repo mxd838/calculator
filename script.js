afe025fa3c4a6ce97a985e4470194fbb88126bf8
mxd838 <mxduran@protonmail.com>
2022-05-15 11:40:06 +0200

    add basic logic architecture

--- script.js
+++ script.js
@@ -18,39 +18,51 @@
 const numbers = document.querySelectorAll('.button[data-number]')
 const operators = document.querySelectorAll('.button[data-operator]')
 const modifications = document.querySelectorAll('.button[data-modif]')
-const display = document.querySelector('display')
+const display = document.querySelector('.display')
 
 // Variables
-let operands = [0,0]
+// -- inputs : operand1, operator, operand2
+let operand1 = ''
+let sign = ''
+let operand2 = ''
 
 
 // Functions
+// -- after the calculcation (precisely, after the sign equal inputed), reset the 3 variables to undefined
 // Add
 // Substract
 // Divide
+// -- display.innerHTML += number.innerText
 // Multiply
 // Percentage
+// Display
+// Convert string to number
+
 
 
 
 // Event listeners
+// -- set the operands and sign to the user inputs 
+// -- call the functions to convert to numbers, calculate and display
 numbers.forEach( number => {
     number.addEventListener('click', ()=> {
         console.log(number.innerText)
-        console.log(typeof number.innerText)
-
+        // if operand1.length = 0, set the input as first digit
+        // if not, concate the next inputs as the following digits
     })
 })
 
 operators.forEach( operator => {
     operator.addEventListener('click', ()=> {
         console.log(operator.innerText)
+        display.innerHTML += operator.innerText
     })
 })
 
 modifications.forEach( modification => {
     modification.addEventListener('click', ()=> {
         console.log(modification.innerText)
+        display.innerHTML += modification.innerText
     })
 })
 
